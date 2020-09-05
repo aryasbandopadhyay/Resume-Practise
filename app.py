095caf1544003290acd952384d913de3195cd679
aryasbandopadhyay <ab9746@srmist.edu.in>
2020-09-05 20:51:38 +0530

    Deploying to heroku

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ from nltk import word_tokenize, pos_tag
 
 import spacy
 nlp = spacy.load('en_core_web_sm')
+from spacy.matcher import Matcher
 
 #spell = SpellChecker()
 #nltk.download('stopwords')
@@ -310,11 +311,11 @@ def uploaded_file(filename):
         sections["Review"]="The Resume is correctly Parsed and Optimal. There may be some room for Improvement"
     if(sections['Score'] >=75 and sections['Score']<90):
         sections["Review"]="The Resume may be Correctly Parsed and Optimal. It is advised to pass DOCX Format in ATS Checker. There is certainly Some Room For Improvement"        
-    t=Tagger()
+    
     count_passive=0
     co_pa=0
     for i in line1:
-        if(t.is_passive(i)):
+        if(is_passive(i)):
             count_passive += 1
     
             
@@ -509,74 +510,18 @@ def extract(text):
     year.sort()
     return(year)
 
-class Tagger:
-    def __init__(self):
-        if os.path.exists("tagger.pkl"):
-            with open('tagger.pkl', 'rb') as data:
-                tagger = load(data)
-            self.tagger = tagger
-        else:
-            tagger = create_tagger()
-            self.tagger = tagger
-            self.save()
-
-    def save(self):
-        with open('tagger.pkl', 'wb') as output:
-            dump(self.tagger, output, -1)
-
-    def tag(self, sent):
-        return self.tagger.tag(sent)
-
-    def tag_sentence(self, sent):
-        """Take a sentence as a string and return a list of (word, tag) tuples."""
-        tokens = nltk.word_tokenize(sent)
-        return self.tag(tokens)
-
-    def is_passive(self, sent):
-        return is_passive(self, sent)
-
-def passivep(tags):
-    """Takes a list of tags, returns true if we think this is a passive
-    sentence.
-
-    Particularly, if we see a "BE" verb followed by some other, non-BE
-    verb, except for a gerund, we deem the sentence to be passive.
-    """
+def is_passive(sentence):
+    matcher = Matcher(nlp.vocab)
+    doc = nlp(sentence)
+    passive_rule = [{'DEP': 'nsubjpass'}, {'DEP': 'aux', 'OP': '*'}, {'DEP': 'auxpass'}, {'TAG': 'VBN'}]
+    matcher.add('Passive', None, passive_rule)
+    matches = matcher(doc)
+    count =0
+    if matches:
+        return True
+    else:
+        return False
     
-    after_to_be = list(dropwhile(lambda tag: not tag.startswith("BE"), tags))
-    nongerund = lambda tag: tag.startswith("V") and not tag.startswith("VBG")
-
-    filtered = filter(nongerund, after_to_be)
-    out = any(filtered)
-
-    return out
-
-def create_tagger():
-    """Train a tagger from the Brown Corpus. This should not be called very
-    often; only in the event that the tagger pickle wasn't found."""
-    train_sents = brown.tagged_sents()
-
-    # These regexes were lifted from the NLTK book tagger chapter.
-    t0 = nltk.RegexpTagger(
-        [(r'^-?[0-9]+(.[0-9]+)?$', 'CD'), # cardinal numbers
-         (r'(The|the|A|a|An|an)$', 'AT'), # articles
-         (r'.*able$', 'JJ'),              # adjectives
-         (r'.*ness$', 'NN'),              # nouns formed from adjectives
-         (r'.*ly$', 'RB'),                # adverbs
-         (r'.*s$', 'NNS'),                # plural nouns
-         (r'.*ing$', 'VBG'),              # gerunds
-         (r'.*ed$', 'VBD'),               # past tense verbs
-         (r'.*', 'NN')                    # nouns (default)
-        ])
-    t1 = nltk.UnigramTagger(train_sents, backoff=t0)
-    t2 = nltk.BigramTagger(train_sents, backoff=t1)
-    t3 = nltk.TrigramTagger(train_sents, backoff=t2)
-    return t3
-
-def is_passive(tagger, sent):
-    tagged = tagger.tag_sentence(sent)
-    tags = map(lambda tup: tup[1], tagged)
-    return bool(passivep(tags))
 
 def check_for_tense(sentence):
     text = word_tokenize(sentence)
